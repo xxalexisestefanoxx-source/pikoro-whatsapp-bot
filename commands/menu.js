4251ca37dcd15409a3c993b89206d817c6912ac2
xxalexisestefanoxx-source <2.48387992e+08+xxalexisestefanoxx-source@users.noreply.github.com>
2026-09-19 22:08:00 +0000

    chore: keep only primary menu command

--- commands/menu.js
+++ commands/menu.js
@@ -2,11 +2,9 @@ import fs from 'node:fs/promises'
 
 const menu = await fs.readFile(new URL('../lib/menu.txt', import.meta.url), 'utf8')
 
-export const commands = [
-  ...['menu', 'menu2', 'menu3', 'menu4'].map((name) => ({
-    name,
-    async execute({ sock, chat, message }) {
-      await sock.sendMessage(chat, { text: menu }, { quoted: message })
-    }
-  }))
-]
+export const commands = [{
+  name: 'menu',
+  async execute({ sock, chat, message }) {
+    await sock.sendMessage(chat, { text: menu }, { quoted: message })
+  }
+}]
